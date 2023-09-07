319a503aeffcaabbb8d8caf6ce2dbebbe9d0310d
Dan Mc <dmg@amp.co>
2023-09-07 16:20:13 +0100

    added accountInfo model

---
+++ src/AccountInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SnowIO\BrightpearlDataModel;
+
+class AccountInfo
+{
+    /** snow contact id */
+    const EVENT_OWNER_ID = 1085; //
+    const DEFAULT_WAREHOUSE = 2;
+    const WHISTL_WAREHOUSE = 6;
+    const WHISTL_WAREHOUSE_LOCATION_DEFAULT = 221;
+    const WHISTL_WAREHOUSE_LOCATION_QUARANTINE = 220;
+    const COST_PRICE_LIST_ID = 8;
+    /** (Magento) channel */
+    const SALES_ORDER_CHANNEL_ID = 2;
+    /** New web order */
+    const SALES_ORDER_STATUS_ID = 2;
+}
